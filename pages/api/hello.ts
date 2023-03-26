9c4681e9a37ab5db7a23a50b7c28fc937223ac47
AndresDiazGualteros <luisandresdiazgualteros22@gmail.com>
2023-03-26 18:44:07 -0500

    pre-deployment vercel

---
+++ pages/api/hello.ts
@@ -0,0 +1,13 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { FC } from "react";
+
+type Data = {
+  name:string;
+}
+
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req:NextApiRequest, res:NextApiResponse<Data>) {
+  res.status(200).json({ name: 'John Doe' })
+}
